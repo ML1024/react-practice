427cc24c286ce78d93e4fd211ea20ec7cfb45ca8
WML <lucy83844@gmail.com>
2024-03-18 12:37:30 +0800

    整体路由设计

--- react-bill-test/src/pages/Layout/index.js
+++ react-bill-test/src/pages/Layout/index.js
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom"
+
 const Layout = () => {
-  return <div>我是layout</div>
+  return (
+    <div>
+      {/* 二级路由的出口，如果没有出口就无法显示二级路由 */}
+      <Outlet /> 
+      我是layout
+    </div>
+  )
 }
 
 
